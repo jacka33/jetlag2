da33cb5992a8c3361370723893b5870e1fdf34bb
Jack <jack@jack.es>
2025-09-14 16:32:34 +0100

    eslint fixes

--- app/calculate/layout.tsx
+++ app/calculate/layout.tsx
@@ -1,7 +1,8 @@
 'use client'
 
 import { useState } from 'react'
-import { Dialog, DialogBackdrop, DialogPanel, TransitionChild } from '@headlessui/react'
+import Image from 'next/image'
+import { Dialog, DialogBackdrop, DialogPanel } from '@headlessui/react'
 import {
   Bars3Icon,
   GlobeEuropeAfricaIcon,
@@ -23,7 +24,7 @@ export default function RootLayout({
     { name: 'About', href: '#', icon: QuestionMarkCircleIcon, current: false },
   ]
 
-  function classNames(...classes) {
+  function classNames(...classes: (string | undefined)[]) {
     return classes.filter(Boolean).join(' ')
   }
 
@@ -41,26 +42,28 @@ export default function RootLayout({
               transition
               className="relative mr-16 flex w-full max-w-xs flex-1 transform transition duration-300 ease-in-out data-closed:-translate-x-full"
             >
-              <TransitionChild>
-                <div className="absolute top-0 left-full flex w-16 justify-center pt-5 duration-300 ease-in-out data-closed:opacity-0">
-                  <button type="button" onClick={() => setSidebarOpen(false)} className="-m-2.5 p-2.5">
-                    <span className="sr-only">Close sidebar</span>
-                    <XMarkIcon aria-hidden="true" className="size-6 text-white" />
-                  </button>
-                </div>
-              </TransitionChild>
+              <div className="absolute top-0 left-full flex w-16 justify-center pt-5 duration-300 ease-in-out data-closed:opacity-0">
+                <button type="button" onClick={() => setSidebarOpen(false)} className="-m-2.5 p-2.5">
+                  <span className="sr-only">Close sidebar</span>
+                  <XMarkIcon aria-hidden="true" className="size-6 text-white" />
+                </button>
+              </div>
 
               {/* Sidebar component */}
               <div className="relative flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-2 dark:bg-gray-900 dark:ring dark:ring-white/10 dark:before:pointer-events-none dark:before:absolute dark:before:inset-0 dark:before:bg-black/10">
                 <div className="relative flex h-16 shrink-0 items-center">
-                  <img
-                    alt="SleepLag"
-                    src="logo.png"
-                    className="h-8 w-auto dark:hidden"
-                  />
-                  <img
+                  <Image
                     alt="SleepLag"
                     src="/logo.png"
+                    width={32}
+                    height={32}
+                    className="h-8 w-auto dark:hidden"
+                  />
+                  <Image
+                    alt="SleepLag"
+                    src="/logo.png"
+                    width={32}
+                    height={32}
                     className="h-8 w-auto not-dark:hidden"
                   />
                 </div>
@@ -107,14 +110,18 @@ export default function RootLayout({
           {/* Sidebar component */}
           <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 dark:border-white/10 dark:bg-black/10">
             <div className="flex h-16 shrink-0 items-center">
-              <img
+              <Image
                 alt="SleepLag"
                 src="/logo.png"
+                width={32}
+                height={32}
                 className="h-8 w-auto dark:hidden"
               />
-              <img
+              <Image
                 alt="SleepLag"
                 src="/logo.png"
+                width={32}
+                height={32}
                 className="h-8 w-auto not-dark:hidden"
               />
             </div>
