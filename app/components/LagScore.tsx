29aac96ef8c68c0ae770eb3054bd9ae52af11d1a
Jack <jack@jack.es>
2025-09-15 10:57:17 +0100

    lagscore placeholder factors

--- app/components/LagScore.tsx
+++ app/components/LagScore.tsx
@@ -1,7 +1,14 @@
-import { MoonIcon } from '@heroicons/react/24/outline'
+import { MoonIcon, ChevronDoubleDownIcon, ChevronDoubleUpIcon, ChevronDoubleRightIcon } from '@heroicons/react/24/outline'
+
+const factors = [
+  { name: 'Flight time', value: "Medium", icon: ChevronDoubleUpIcon, iconColour: "text-red-600" },
+  { name: 'Time difference', value: "Large", icon: ChevronDoubleRightIcon, iconColour: "text-gray-600" },
+  { name: 'Direction', value: "Eastbound", icon: ChevronDoubleDownIcon, iconColour: "text-green-600" },
+  { name: 'Departure time', value: "Late night", icon: ChevronDoubleUpIcon, iconColour: "text-red-600" },
+]
 
 const stats = [
-  { id: 1, name: 'LagScore', score: '71', comment: '(Moderate jetlag)', icon: MoonIcon },
+  { id: 1, name: 'LagScore', score: '71', comment: 'Moderate jetlag', factors: factors, icon: MoonIcon },
 ]
 
 function classNames(...classes) {
@@ -16,29 +23,37 @@ export default function LagScore() {
         {stats.map((item) => (
           <div
             key={item.id}
-            className="relative overflow-hidden rounded-lg bg-white px-4 pt-5 pb-12 shadow-sm sm:px-6 sm:pt-6 dark:bg-gray-800/75 dark:inset-ring dark:inset-ring-white/10"
+            className="relative overflow-hidden pt-5 pb-12 sm:pt-6"
           >
             <dt>
               <div className="absolute rounded-md bg-blue-800 p-3">
                 <item.icon aria-hidden="true" className="size-6 text-white" />
               </div>
-              <p className="ml-16 truncate text-sm font-medium text-gray-500 dark:text-gray-400">{item.name}</p>
+              <p className="ml-16 truncate text-sm font-medium text-blue-800 dark:text-blue-400">{item.name} <span className='text-xs text-gray-500'>(Lower is better)</span></p>
             </dt>
-            <dd className="ml-16 flex items-baseline pb-6 sm:pb-7">
+            <dd className="ml-16 flex items-baseline pb-4 sm:pb-4">
               <p className="text-2xl font-semibold text-gray-900 dark:text-white">{item.score}<span className="text-sm">/100</span></p>
-              <p className="ml-2 text-medium font-semibold text-gray-900 dark:text-white">{item.comment}</p>
-
-              <div className="absolute inset-x-0 bottom-0 bg-gray-50 px-4 py-4 sm:px-6 dark:bg-gray-700/20">
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-medium text-blue-600 hover:text-blue-500 dark:text-blue-400 dark:hover:text-blue-300"
-                  >
-                    Personalise your score, jetlag plan and strategy
-                  </a>
-                </div>
-              </div>
+              <p className="ml-2 text-medium font-semibold text-gray-700 dark:text-white">{item.comment}</p>
             </dd>
+            <div className="relative mb-8">
+              {item.factors.map((factor) => (
+                <p className="text-medium font-semibold text-gray-900 dark:text-white" key={factor.name}>
+                  {factor.name}: {factor.value}{' '}
+                  <factor.icon aria-hidden="true" className={`inline size-4 ${factor.iconColour || ''}`} />
+                </p>
+              ))
+              }
+            </div>
+            <div className="absolute inset-x-0 bottom-0 bg-gray-50 px-4 py-4 sm:px-6 dark:bg-gray-700/20">
+              <div className="text-sm">
+                <a
+                  href="#"
+                  className="font-medium text-blue-600 hover:text-blue-500 dark:text-blue-400 dark:hover:text-blue-300"
+                >
+                  Personalise your score, jetlag plan and strategy
+                </a>
+              </div>
+            </div>
           </div>
         ))}
       </dl>
