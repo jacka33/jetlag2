0d31ce0c3f8fe0e716432a06cf50af38f7ecc899
Jack <jack@jack.es>
2025-09-17 19:49:00 +0100

    fix ts error

--- app/components/results/insights/Insights.tsx
+++ app/components/results/insights/Insights.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Insights = (response) => {
+const Insights: React.FC = (response: unknown) => {
   console.log('AI Insights response:', response);
   return (
     <div>check console log for ai insights</div>
